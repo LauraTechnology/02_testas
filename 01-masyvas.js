2aa7592b3dcd4fa7afc447398545c950a89806dc
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-06 13:50:39 +0300

    2 uzduotis

--- 01-masyvas.js
+++ 01-masyvas.js
@@ -6,3 +6,26 @@ console.log(mas1);
 mas1.lenght = length
 
 console.log(mas1.lenght);
+
+console.log('----------');
+
+//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
+// iteracijoje atspausdinti indeksą ir jo reikšmę;
+
+const mas2 = [13, '785', 92, 'labas', 522, 6984];
+
+console.log(mas2);
+
+console.log(mas2[0]);
+console.log(mas2[1]);
+console.log(mas2[2]);
+console.log(mas2[3]);
+console.log(mas2[4]);
+console.log(mas2[5]);
+
+console.log('----------');
+
+ 
+//3. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
+// iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė yra lyginė (porinė)
+const mas3 = [889, 52, 698, 752, 9333, 7];
