b45f9504abbe026fa0e9ce2af4830020646ec9c2
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-06 13:45:46 +0300

    First commit

---
+++ 01-masyvas.js
@@ -0,0 +1,8 @@
+//1. Duotas masyvas, console.log atspausdinti masyvo ilgį
+const mas1 = [12, 5, 9, 7, 5, 4];
+
+console.log(mas1);
+
+mas1.lenght = length
+
+console.log(mas1.lenght);
---
+++ 02-test.js
@@ -0,0 +1,3 @@
+//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
+// iteracijoje atspausdinti indeksą ir jo reikšmę;
+const mas2 = [13, '785', 92, 'labas', 522, 6984];
