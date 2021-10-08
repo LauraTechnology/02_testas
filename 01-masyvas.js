5c9224ba69cca510b2d68e05a3ec07738e220ac2
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-08 10:08:40 +0300

    Sunumeravau užduotis

--- 01-masyvas.js
+++ 01-masyvas.js
@@ -1,3 +1,4 @@
+console.log('-----1-----');
 //1. Duotas masyvas, console.log atspausdinti masyvo ilgį
 
 const mas1 = [12, 5, 9, 7, 5, 4];
@@ -6,8 +7,7 @@ console.log(mas1);
 
 console.log(mas1.length);
 
-console.log('----------');
-
+console.log('-----2-----');
 //2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę;
 
@@ -30,9 +30,7 @@ for (let i = 0; i < mas2.length; i++) {
 //console.log(mas2[4]);
 //console.log(mas2[5]);
 //
- console.log('----------');
-
- 
+ console.log('-----3-----');
 //3. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė yra lyginė (porinė)
 const mas3 = [889, 52, 698, 752, 9333, 7];
@@ -52,38 +50,45 @@ iteracija += mas3[iteracijaIndex++];
 console.log(iteracija);
 
 
-console.log('----------');
-
-
-
+console.log('-----4-----');
 //4. Duotas masyvas, masyvą iteruoti ciklu forEach ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė prasideda raide A arba a
 const mas4 = ['Agurkas', 'Bananas', 'arbūzas', 'Bebras', 'Voverė', 'Ąžuolas', 'Mažas triušis'];
  
 
-
+console.log('-----5-----');
 //5. Duotas stringas, suskaičiuoti kiek stringe yra raidžių z
 const str1 = 'Storas zebras, bėga nuo zuulu genties nedžiotojų.';
  
 
 
 
-
+console.log('-----6-----');
 //6. Duotas Setas, į kurį reikia pridėti reikšmes (stringus) 'Labas' ir 'Ate' ir console.log
 // atspausdinti Seto ilgį
 const set1 = new Set(['Labas', 'Vakaras', 'Rytas', 'Valio']);
  
+
+console.log('-----7-----');
 //7. Suprototaipinti standartinį String tipo objektą, pridedant metodą lastCharIs(), 
 // kuris grąžintų paskutinį stringo simbolį. Pademonstruoti veikimą su stringu 'Ku Ku';
 const str2 = 'Ku ku';
 // console.log(str2.lastCharIs()) po prototaipinimo eilutė turi veikti
  
+
+console.log('-----8-----');
 //8. Duotas objektas, kurį reikia iteruoti, kiekvienoje iteracijoje išvedant savybę
 // ir jos reikšmę
 const obj1 = { labas: 'vakaras', laba: 'diena', labs: 'ryts' };
  
+
+console.log('-----9-----');
 //9. Sukurti klasę Grybas. Klasėje sukurti savybė tipas. Iš klasės sukurti
 // 3 Grybo objektus, savybei tipas priskiriant "Baravykas", "Ūmedė" ir "Lepšis"
+
+
+
+
 
 
 
