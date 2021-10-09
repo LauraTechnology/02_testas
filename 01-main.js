ff7529f9083998c759b70de403d5eaa6966a1450
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-09 11:16:29 +0300

    console.log(indeksas, jo reiksme)

--- 01-main.js
+++ 01-main.js
@@ -9,8 +9,7 @@ console.log(mas1.length);
 
 
 console.log('-----2-----');
-//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
-// iteracijoje atspausdinti indeksą ir jo reikšmę;
+//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje iteracijoje atspausdinti indeksą ir jo reikšmę;
 
 const mas2 = [13, '785', 92, 'labas', 522, 6984];
 
--- i-value.js
+++ i-value.js
@@ -1,7 +1,52 @@
-//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
-// iteracijoje atspausdinti indeksą ir jo reikšmę;
+//2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje iteracijoje atspausdinti indeksą ir jo reikšmę;
 //
 //  atlikti 10 ciklu tokiu pačiu principu.
 
+console.log('1__________');
+
 const months = ['January', 'Fabruary', 'March', 'August', 'September', 'October', 'November'];
 
+        console.log(months);
+
+
+
+for (let i = 0; i < months.length; i++ ) {
+    console.log(months[i], i);
+}
+
+
+console.log('2__________');
+
+const maistas = ['Duon', 'Pien', 'Sviest', 'Vais', 'Darz', 'Seklos', 'Riesutai', 'Ankst'];
+
+for (let i = 0; i < maistas.length; i++) {
+    console.log(maistas[i], i);
+}
+
+
+console.log('3__________');
+
+const desertai = ['ice', 'cream', 'cookie', 'jam', 'waffle', 'choco', 'coco'];
+//        console.log(desertai);
+
+for (let i = 0; i < desertai.length; i++) {
+    console.log(desertai[i], i);
+}
+
+
+
+
+
+console.log('4__________');
+
+const orai = ['Geras', 'Blogas', 'Malonus', 'Nemalonus'];
+
+console.log('5__________');
+
+const spalvos = ['Raud', 'Zal', 'Gelt', 'Mel', 'Oran', 'Auks', 'Sidab'];
+
+console.log('6__________');
+console.log('7__________');
+console.log('8__________');
+console.log('9__________');
+console.log('10_________');
